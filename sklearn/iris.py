656947241da32fc57e160b0804a9040c420cb4b8
caio-muniz <caaioomunizz@gmail.com>
2026-08-22 14:38:31 -0300

    feat: add sklearn

---
+++ sklearn/iris.py
@@ -0,0 +1,20 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+
+iris = load_iris()
+
+X = iris.data
+y = iris.target
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42, stratify=y
+)
+
+print(X_train.shape)
+print(X_test.shape)
+print(y_train.shape)
+print(y_test.shape)
+
+
+
+
