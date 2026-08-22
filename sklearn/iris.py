96547e66cbb7275e1c404e229234dd13b144d4e1
caio-muniz <caaioomunizz@gmail.com>
2026-08-22 15:46:51 -0300

    feat: enhance KNN evaluation with cross-validation and updates

--- sklearn/iris.py
+++ sklearn/iris.py
@@ -2,6 +2,8 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import cross_val_score
+import numpy as np
 
 iris = load_iris()
 
@@ -9,7 +11,7 @@ X = iris.data
 y = iris.target
 
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42, stratify=y
+    X, y, test_size=0.2, random_state=10, stratify=y
 )
 
 def evaluateKNN(k, X_train, y_train, X_test, y_test):
@@ -22,11 +24,17 @@ def evaluateKNN(k, X_train, y_train, X_test, y_test):
      
     return accuracy
 
-i = 1
-while i <= 10:
-    print(f"k = {i}: {evaluateKNN(i, X_train, y_train, X_test, y_test)}")
-    i+=1
+def evaluateKNNWithCV(k, X_train, y_train):
+    model = KNeighborsClassifier(n_neighbors = k)
 
+    scores = cross_val_score(model, X_train, y_train)
 
+    mean = np.mean(scores)
 
+    return mean
 
+model = KNeighborsClassifier(n_neighbors = 7)
+model.fit(X_train, y_train)
+predict = model.predict(X_test)
+accuracyFinal = accuracy_score(y_test, predict)
+print(accuracyFinal)
