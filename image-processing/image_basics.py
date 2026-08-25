8054a05554079a33c8b08886782ad339f330e6aa
caio-muniz <caaioomunizz@gmail.com>
2026-08-25 15:15:30 -0300

    feat: enhance image processing with pixel info and channels
    Added functions to retrieve pixel information and display color channels.

--- image-processing/image_basics.py
+++ image-processing/image_basics.py
@@ -1,10 +1,23 @@
 from PIL import Image
 import matplotlib.pyplot as plt
+import numpy as np
 
 image = Image.open("imagem.jpg")
 
-plt.imshow(image)
-plt.show()
+def getPixelInfo(image, x, y):
+    result = image.getpixel((x,y))
 
-print(image.size)
-print(image.mode)
+    return result
+
+imageArray = np.array(image)
+
+redChannel = imageArray[:, :, 0]
+greenChannel = imageArray[:, :, 1]
+blueChannel = imageArray[:, :, 2]
+
+print(redChannel.shape)
+print(greenChannel.shape)
+print(blueChannel.shape)
+
+plt.imshow(redChannel, cmap="gray")
+plt.show()
