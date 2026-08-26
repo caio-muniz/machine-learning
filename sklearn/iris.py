272868f6ae164a73ac9315a0d517a06723332489
caio-muniz <caaioomunizz@gmail.com>
2026-08-26 14:39:28 -0300

    feat: integrate Fashion MNIST dataset with KNN evaluation

--- sklearn/iris.py
+++ sklearn/iris.py
@@ -3,16 +3,26 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import cross_val_score
+from tensorflow.keras.datasets import fashion_mnist
+from utils.image_processing import (
+    getPixelInfo,
+    resizeImage,
+    normalizeImage,
+    flattenImages,
+    preprocessImages
+)
 import numpy as np
 
 iris = load_iris()
+(X_train, y_train), (X_test, y_test) = fashion_mnist.load_data()
+
 
 X = iris.data
 y = iris.target
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=10, stratify=y
-)
+#X_train, X_test, y_train, y_test = train_test_split(
+#    X, y, test_size=0.2, random_state=10, stratify=y
+#)
 
 def evaluateKNN(k, X_train, y_train, X_test, y_test):
     model = KNeighborsClassifier(n_neighbors = k)
@@ -33,8 +43,11 @@ def evaluateKNNWithCV(k, X_train, y_train):
 
     return mean
 
+X_train_processed = preprocessImages(X_train)
+X_test_processed = preprocessImages(X_test)
+
 model = KNeighborsClassifier(n_neighbors = 7)
-model.fit(X_train, y_train)
-predict = model.predict(X_test)
+model.fit(X_train_processed[:10000], y_train[:10000])
+predict = model.predict(X_test_processed[:10000])
 accuracyFinal = accuracy_score(y_test, predict)
 print(accuracyFinal)
