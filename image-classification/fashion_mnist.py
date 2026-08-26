6c4d6ba19c7871829aa928e325788ed6b2c8e72d
caio-muniz <caaioomunizz@gmail.com>
2026-08-26 14:18:49 -0300

    feat: implement image flattening and normalization functions
    Added functions to flatten and normalize images for preprocessing.

--- image-classification/fashion_mnist.py
+++ image-classification/fashion_mnist.py
@@ -12,7 +12,29 @@ def showSampleImages(X, y):
         plt.show()
         i+=1
 
+def flattenImages(X):
+    arr = np.array(X)
+    flat = arr.reshape(arr.shape[0], -1)
+    
+    return flat
 
+def normalizeImage(imageArray):
+    normalizedArray = imageArray/255
 
-#plt.imshow(X_train[4], cmap="gray")
-#plt.show()
+    return normalizedArray
+    
+def preprocessImages(X):
+    flatX = flattenImages(X)
+    normalizedX = normalizeImage(flatX)
+    
+    return normalizedX
+    
+X_train_processed = preprocessImages(X_train)
+X_test_processed = preprocessImages(X_test)
+
+print(X_train_processed.shape)
+print(X_test_processed.shape)
+
+print(np.min(X_train_processed))
+print(np.max(X_train_processed))
+
